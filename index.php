9f4e9b575d030e4f97bc7586bcd52e5d841f2f6a
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-10 19:05:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
   <head></head>
 <body>
 
-</h1>Hi surbhi</h1>
+<h1>Hi surbhi</h1>
+  <h2>This website is for you</h2>
 <br>
 <img src="suru.png">
 
-<h1>You can put ur artistic photos.. here</h1>
+<h1>You can put ur artistic supercool paintings... here</h1>
 <img src="suru1.jpg">
-<p>This website is for you</p>
 
+<h3>created by Avi</h3>
 </body>
 </html>
