ffcb8b882a8d028d3adc7237f2eee2b3aed0cf2a
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-10 23:43:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,15 @@
 <h1>Hi surbhi</h1>
   <h2>This website is for you</h2>
 <br>
-<img src="suru.png">
+<img src="suru1.jpg">
 
 <h1>You can put ur artistic supercool paintings... here</h1>
-<img src="suru1.jpg">
+  
+<form action="newup.php" method="post" enctype="multipart/form-data">
+<input type="file" name="file" id="file"><br><br>
+<input type="submit" value="submit" name="submit">
+</form> 
+
 
 <h3>created by Avi</h3>
 </body>
